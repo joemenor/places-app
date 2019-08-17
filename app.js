c8c17df1d8962725b6a914a0de4691753225ed41
joemenor <48729377+joemenor@users.noreply.github.com>
2019-08-17 16:37:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const http = require('http');
+var fs = require('fs');
 const port = process.env.PORT || 3000
 
 const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/html');
-  res.end('<h1>Hello World</h1>');
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+    res.end();
+  });
 });
 
 server.listen(port,() => {
